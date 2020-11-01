417f0125417c23a311c218b7fe3d388327701720
Saleh Parsa <salehp@gmail.com>
2020-11-01 23:13:01 +0100

    setup k8s cluster with github action

---
+++ main.tf
@@ -0,0 +1,18 @@
+/**
+Since provider has moved to linode/linode for users on Terraform 0.13 or greater,
+to make it compatible with terraform 0.13 or greater,linode/linode is used for source 
+instead of hashicorp/linode in required_providers.
+**/
+terraform {
+  required_version = ">=0.12.21"  
+  required_providers {
+    linode = {
+      version = ">= 1.13"
+      source = "linode/linode"
+    }
+  }
+}
+
+provider "linode" {
+  token = var.token
+}
